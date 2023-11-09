98889ac79d7194110355fd78b973193cd53335d5
EnyelSequeira <enyelsequeira@hotmail.com>
2023-11-09 22:34:50 +0800

    feat: initial setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,22 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import Head from "next/head";
+import { AppProps } from "next/app";
+import { createEmotionSsrAdvancedApproach } from "tss-react/next/pagesDir";
+import { ColorSchemeScript, MantineProvider } from "@mantine/core";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import "@mantine/core/styles.css";
+import { theme } from "@/theme";
+
+const { augmentDocumentWithEmotionCache, withAppEmotionCache } =
+  createEmotionSsrAdvancedApproach({ key: "css" });
+
+export { augmentDocumentWithEmotionCache };
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <MantineProvider theme={theme}>
+      <Component {...pageProps} />;
+    </MantineProvider>
+  );
 }
+//You can also pass your custom App if you have one.
+export default withAppEmotionCache(App);
